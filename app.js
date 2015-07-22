13939a769cdaabd0aef516ffeba179f943aa4eef
Marc Christophe <m@xtof.net>
2015-07-22 14:02:01 -0700

    modifying my app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express'),
   config = require('./config/config');
 
@@ -7,5 +5,19 @@ var app = express();
 
 require('./config/express')(app, config);
 
-app.listen(config.port);
+// app.listen(config.port, function(){
+//   console.log('funk');
+// });
 
+//var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
